9b2c9f7827125d108fa1e724626bbd256fd3d507
tuss4 <tuss4dbfn@gmail.com>
2018-12-22 05:54:44 -0600

    Let's see if we can get a fail.

--- tests.py
+++ tests.py
@@ -8,4 +8,5 @@ def test_ping():
     response = client.get('/')
     data = json.loads(response.content)
     assert response.status_code == 200
-    assert data['ping'] == "Pong yo."
+    # assert data['ping'] == "Pong yo."
+    assert data['ping'] == "MY GUY"
